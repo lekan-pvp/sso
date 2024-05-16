99d09c36dc34d1b439c033f1e9fa0e50b98208a9
Vlad Popov <lekan.lekan@gmail.com>
2024-05-16 22:47:27 +0300

    slog added

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,15 +1,60 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/lekan-pvp/sso/internal/config"
+	"github.com/lekan-pvp/sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	// TODO: loger initialization
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.Any("cfg", cfg),
+	)
 
 	// TODO: application initialization
 
 	// TODO: run grpc server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
