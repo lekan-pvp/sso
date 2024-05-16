abf7b8291657a0d41d664dc7b09499820c2027a0
Vlad Popov <lekan.lekan@gmail.com>
2024-05-16 17:59:10 +0300

    config initialization

---
+++ cmd/sso/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/lekan-pvp/sso/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	// TODO: loger initialization
+
+	// TODO: application initialization
+
+	// TODO: run grpc server
+}
